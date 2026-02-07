d414cf293179981e806d48e155e00ae12b404686
isaac <tiezic@gmail.com>
2026-02-06 18:09:53 -0800

    comments

--- mergeMainHW2.java
+++ mergeMainHW2.java
@@ -11,27 +11,27 @@ public class mergeMainHW2 {
 
     }
 
-    int[] mergeArrays(int[] array1, int[] array2) {
+    int[] mergeArrays(int[] array1, int[] array2) {                //create a method that takes in both sorted arrays
         int mergedArraySize = array1.length + array2.length;
-        int[] mergedArray = new int[mergedArraySize];
-        int i = 0, j = 0, k = 0;
+        int[] mergedArray = new int[mergedArraySize];              //create new array with size of array 1 + array 2
+        int i = 0, j = 0, k = 0;                                   //initialize counters
         
-        while (i < array1.length && j < array2.length) {
+        while (i < array1.length && j < array2.length) {           //while j and i counters are not over the length of the array:
 
-            if (array1[i] < array2[j]) {
-                mergedArray[k] = array1[i];
-                i++;
-            } else {
-                mergedArray[k] = array2[j];
+            if (array1[i] < array2[j]) {                           //if the current value of array 2 is bigger than array 1, then set  
+                mergedArray[k] = array1[i];                        //the next index of mergedArray to array 1's value, increment array 1's
+                i++;                                               //counter, i, then increment mergedArray's counter, k
+            } else {                                               //else if array 1 value is bigger than array 2, set mergedArray index to
+                mergedArray[k] = array2[j];                        //array 2, then increment array 2 counter as well as mergedArray counter
                 j++;
             }
             k++;
 
         }
 
-        while (i < array1.length) {
+        while (i < array1.length) {                                //eventually, either i or j will go over their respective array lengths
 
-            mergedArray[k] = array1[i];
+            mergedArray[k] = array1[i];                            //and the rest of the array's values must be copied to mergedArray
             i++;
             k++;
 
@@ -48,3 +48,4 @@ public class mergeMainHW2 {
 
     } //end mergeArrays()
 } //end mergeMain
+
